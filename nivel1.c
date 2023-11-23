f046d8be9b29c7968490d2eff945b6a862cbc38e
Legoruu <13arnauvidal@gmail.com>
2023-11-23 12:52:51 +0100

    Iniciado git para no mas bugs

---
+++ nivel1.c
@@ -0,0 +1,166 @@
+#include "shell.h"
+
+char *imprimir_prompt()
+{
+    char *USER = getenv("USER");
+    char *HOME = getenv("HOME");
+    char CWD[_MAX_PATH];
+
+    if (!(getcwd(CWD, sizeof(CWD)) != NULL)) {
+        perror("getcwd() error");
+    }
+    // TODO: LIMPIAR HOME
+
+    printf("%s%s:~%s%s%c%s", ORANGE, USER, CREAM, CWD, PROMPT, BASE_COLOR);
+}
+
+//cuando devuelve error?
+int parse_args(char **args, char *line)
+{
+    int num_tokens = 0;
+    char *del = " \t\n\r";
+    char *token = strtok(line, del);
+    while (token != NULL)
+    {
+        if (token[0] != '#')
+        {
+            printf("%s - %d\n", token, num_tokens);
+            args[num_tokens] = strdup(token);
+            num_tokens++;
+        }
+        token = strtok(NULL, del);
+    }
+    args[num_tokens] = NULL;
+    printf("Total tokens: %d\n", num_tokens);
+}
+
+int internal_cd(char** args)
+{
+    if(args[1] == NULL) {
+        printf("CD A HOME\n");
+        if(chdir(getenv("HOME")) != 0) {
+            perror("internal_cd home error");
+        }
+    }
+    else if(args[2] == NULL) {
+        if(chdir(args[1]) != 0) {
+            perror("internal_cd error");
+        }
+    }
+    else {
+        //TODO: implementar cd avanzado
+        if(strchr(args[1], '\'') || strchr(args[1], '"')) {
+            
+        }
+    }
+    printf("LLAMASTE A CD\n");
+}
+
+int internal_bg(char** args)
+{
+    printf("LLAMASTE A BG\n");
+}
+
+int internal_fg(char** args)
+{
+    printf("LLAMASTE A FG\n");
+}
+
+int internal_source(char** args)
+{
+    printf("LLAMASTE A SOURCE\n");
+}
+
+int internal_jobs(char** args)
+{
+    printf("LLAMASTE A JOBS\n");
+}
+
+int internal_export(char** args)
+{
+    if(args[2] != NULL) {
+        fprintf(stderr, "internal_export() -> demasiados argumentos\n");
+        return EXIT_FAILURE;
+    }
+    char* key;
+    char* new_value;
+    char* del = "=";
+    char* token = strtok(args[1], del);
+    if (token == NULL) {
+        fprintf(stderr, "internal_export() -> args[1] erroneo");
+        return EXIT_FAILURE;
+    }
+    key = token;
+    new_value = strtok(NULL, "");
+    if (new_value == NULL) {
+        fprintf(stderr, "internal_export() -> args[1] erroneo");
+        return EXIT_FAILURE;
+    }
+    char* last_value = getenv(key);
+    printf("internal_export() -> nombre: %s\n", key);
+    printf("internal_export() -> antiguo valor: %s\n",last_value);
+    printf("internal_export() -> nuevo valor: %s\n", new_value);
+    if(setenv(key, new_value, 1) != 0) {
+        perror("internal_export() -> setenv");
+        return EXIT_FAILURE;
+    }
+    printf("LLAMASTE A EXPORT\n");
+    return EXIT_SUCCESS;
+}
+
+int check_internal(char **args)
+{
+    if(strcmp(args[0], "cd") == 0) {
+        internal_cd(args);
+    } else if(strcmp(args[0], "export") == 0){
+        internal_export(args);
+    } else if(strcmp(args[0], "source") == 0){
+        internal_source(args);
+    } else if(strcmp(args[0], "jobs") == 0){
+        internal_jobs(args);
+    } else if(strcmp(args[0], "fg") == 0){
+        internal_fg(args);
+    } else if(strcmp(args[0], "bg") == 0){
+        internal_bg(args);
+    } else if(strcmp(args[0], "exit") == 0){
+        printf("Hasta la proxima!\n");
+        exit(0);
+    }
+}
+
+int execute_line(char *line)
+{
+    char *args[MAX_ARGS];
+    if (parse_args(args, line) == -1)
+    {
+        perror("Error al parsear argumentos");
+        return -1;
+    }
+    if (sizeof(args) < 0)
+    {
+        perror("No hay argumentos");
+        return -1;
+    }
+    check_internal(args);
+    return 1;
+}
+
+void main()
+{
+    while (1)
+    {
+        char *line = malloc(COMMAND_LINE_SIZE);
+        imprimir_prompt();
+        if (fgets(line, COMMAND_LINE_SIZE, stdin) == NULL)
+        {
+            printf("\n%sNO FUIMOOO%s", ORANGE, BASE_COLOR);
+            exit(0);
+        }
+        if (line != NULL)
+        {
+            line[strlen(line) - 1] = '\0';
+            execute_line(line);
+        }
+        free(line);
+    }
+}
---
+++ shell.h
@@ -0,0 +1,23 @@
+#define COMMAND_LINE_SIZE 1024
+#define MAX_ARGS 32
+#define N_JOBS 64
+#define _MAX_PATH 260
+#define PROMPT '$'
+
+#define CREAM "\033[38;2;245;222;179m"
+#define RED "\033[1;31m"
+#define ORANGE "\033[1;34m"
+#define BASE_COLOR "\033[0m"
+
+#include <stdio.h>     /* para printf en depurarión */
+#include <string.h>    /* para funciones de strings  */
+#include <stdlib.h>    /* Funciones malloc(), free(), y valor NULL */
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+struct info_job {
+    pid_t pid;
+    char estado;
+    char cmd[COMMAND_LINE_SIZE];
+};
